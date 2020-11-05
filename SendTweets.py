2269e73680236517d22afbee57b00d6558d56909
ccummings1282 <ccummings1282@gmail.com>
2020-11-05 00:06:51 -0600

    Started reading data from feed.

--- SendTweets.py
+++ SendTweets.py
@@ -6,22 +6,39 @@ consumer_secret_key = credentials.API_SECRET_KEY
 access_token = credentials.ACCESS_TOKEN
 access_token_secret = credentials.ACCESS_TOKEN_SECRET
 
-# consumer_key = "49RYz6lxn94GlZx6BOVmafFCV"
-# consumer_secret_key = "CywArKb9kyFYk2lsVnsIlxDsM9RBrIpO89SUDGh6PQGJbdqlxF"
-# access_token = "3893377102-MvBTAExOJR698ggpJDI0zUuWDZHsmHSxauOqfP2"
-# access_token_secret = "i8sHdoncY7bIXX4zmRDzEieCnVWefKkgFnqClm54sfXXS"
-
 def tweet_update():
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    test_tweet = "Lol I made a bot"
-    for i in range(50):
-        test_tweet = test_tweet + "lol"
-        api.update_status(test_tweet)
-        print("hi")
+
+
+    # mentions = api.mentions_timeline()
+    #760840544117334016
+    #760840544117334016
+    # mentions['id'] = mentions.__dict__
+
+    #tweet['id']
+    # print(mentions)
+
+
+
+    tweets = api.mentions_timeline()
+    for t in tweets:
+
+        # hashtags = [i['text'].lower() for i in t.__dict__['entities']['hashtags']]
+        print("retweeted tweet of : "+t.in_reply_to_screen_name)
+        # print("retweeted tweet of : "+t.__dict__['source'])
+
+
+    # test_tweet = "Lol I made a bot"
+    # for i in range(5):
+    #     test_tweet = test_tweet + "lol"
+    #     api.update_status(test_tweet)
+    #     print("hi")
 
 
 if __name__ == "__main__":
     tweet_update()
+
+    # to run:   python .\SendTweets.py
